80f30ee35ace29af5a3febc0e505d130a3f3ad5f
wzx <wzx@qq.com>
2023-09-18 16:37:46 +0800

    Add '()' function but only for one '()'

--- Lab1.cpp
+++ Lab1.cpp
@@ -9,6 +9,10 @@ void MyFunc(stack<int> &stk, stack<char> &opt)
 {
     char ch = opt.top();
     opt.pop();
+    if (ch == '(')
+    {
+        return;
+    }
     int b = stk.top();
     stk.pop();
     int a = stk.top();
@@ -44,23 +48,26 @@ int evalRPN(string str)
     stack<int> stk;
     stack<char> opt;
     unordered_map<char, int> p;
-    p['+'] = 0;
-    p['-'] = 0;
-    p['*'] = 1;
-    p['/'] = 1;
+    p[')'] = 0;
+    p['+'] = 1;
+    p['-'] = 1;
+    p['*'] = 2;
+    p['/'] = 2;
+    p['('] = 3;
+
+    bool flag = false;
     for (int i = 0; i < len; ++i)
     {
-        int temp = 0;
-        while (i < len && str[i] - '0' < 0 || str[i] - '0' > 9)
+        if (str[i] - '0' >= 0 && str[i] - '0' <= 9)
         {
-            ++i;
+            int temp = 0;
+            while (i < len && str[i] - '0' >= 0 && str[i] - '0' <= 9)
+            {
+                temp = temp * 10 + (str[i] - '0');
+                ++i;
+            }
+            stk.push(temp);
         }
-        while (i < len && str[i] - '0' >= 0 && str[i] - '0' <= 9)
-        {
-            temp = temp * 10 + (str[i] - '0');
-            ++i;
-        }
-        stk.push(temp);
         if (i >= len)
         {
             break;
@@ -68,14 +75,36 @@ int evalRPN(string str)
         while (opt.size() > 0)
         {
             char ch = opt.top();
-            if (p[ch] >= p[str[i]])
+            int ta = p[str[i]];
+            int tb = p[ch];
+            if (flag && ch == '(')
+            {
+                ta += p['('];
+                 
+            }
+            else if (flag && str[i] != ')')
+            {
+                ta += p['('];
+                tb += p['('];
+            }
+
+            if (ta <= tb)
             {
                 MyFunc(stk, opt);
-            } 
+            }
             else
             {
                 break;
             }
+        }
+        if (str[i] == ')')
+        {
+            flag = false;
+            continue;
+        }
+        if (str[i] == '(')
+        {
+            flag = true;
         }
         opt.push(str[i]);
     }
